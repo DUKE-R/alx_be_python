8d54f169c1169e1e17453d798d72355529128fed
Richard <odhiamboritchy@gmail.com>
2024-09-12 11:14:12 +0300

    simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Define the variables
+principal = 1000  #amount in dollars
+rate = 0.05       #anual interest as a decimal
+time = 3          #time in years
+
+#Perform the operation
+interest = principal * rate * time
+
+#Print the result
+print(f"The simple interest is: {interest}")
